7efc7c13d816e14e8b41a0b75b2e203f1277b83e
PiyadinTony <facepalm54321@gmail.com>
2023-09-21 12:15:07 +0300

    [2]

--- src/test/java/ui/TestBase.java
+++ src/test/java/ui/TestBase.java
@@ -1,7 +1,5 @@
 package ui;
 
-import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
 import ui.helpers.WebDrivers;
 
@@ -10,8 +8,8 @@ public class TestBase extends WebDrivers {
     public String WayToScreen = "C:\\Users\\user\\IdeaProjects\\FinalHomework\\src\\test\\java\\ui\\helpers\\screen\\myscreenshot.png";
     public String Video = "https://www.youtube.com/watch?v=Llyp-bN-wBE&ab_channel=TESFAN";
 
-    public static String CssShare = "button.oxd-glass-button:nth-child(2)";
 
     public void getUrl(WebDriver driver){
-        driver.get(URL);}
+        driver.get(URL);
+    }
 }
