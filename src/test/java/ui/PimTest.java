8d65ae0d80c2ab3ab8afc5db2a8ec5a65fbbbc1b
PiyadinTony <facepalm54321@gmail.com>
2023-10-02 13:56:36 +0300

    7х work on work(super in PimTest)

--- src/test/java/ui/PimTest.java
+++ src/test/java/ui/PimTest.java
@@ -4,12 +4,10 @@ import com.codeborne.selenide.testng.ScreenShooter;
 import io.qameta.allure.*;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import ui.helpers.WebDrivers;
 import ui.pages.LoginPage;
 import ui.pages.PimPage;
 
 import static io.qameta.allure.SeverityLevel.CRITICAL;
-import static io.qameta.allure.SeverityLevel.NORMAL;
 
 @Listeners({ScreenShooter.class})
 public class PimTest extends PimPage {
